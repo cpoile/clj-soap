6a9c15fac0b79404d89223809a1fbd5bda3764ca
Christopher Poile <poile@edwards.usask.ca>
2014-03-22 12:53:23 -0600

    upgrade dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.incubator "0.1.2"]
+                 [org.clojure/core.incubator "0.1.3"]
                  [org.apache.axis2/axis2-adb "1.6.2"]
                  [org.apache.axis2/axis2-transport-http "1.6.2"]
                  [org.apache.axis2/axis2-transport-local "1.6.2"]]
